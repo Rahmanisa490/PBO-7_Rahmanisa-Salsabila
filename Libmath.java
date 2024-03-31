5b8e38c1f0ea9a626bc01a1badd69208cbf84651
Rahmanisa Salsabila <124027476+Rahmanisa490@users.noreply.github.com>
2024-03-31 22:00:55 +0700

    Create Libmath.java

---
+++ Libmath.java
@@ -0,0 +1,13 @@
+Public class Libmath<T extends Number,U extends Number> {
+    Private T angka1;
+    private U angka2;
+
+    public Libmath (T angka, U angka2){
+        this.angka1 = angka 1;
+        this.angka2 = angka2;
+
+    }
+    Public void add() {
+        System.out.println(angka1.intValue() + (Integer) angka2.intValue());
+    }
+}
