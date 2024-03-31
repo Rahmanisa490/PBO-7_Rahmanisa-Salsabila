6635de209b26e3545dd917dfe0665408c5bdc969
Rahmanisa Salsabila <124027476+Rahmanisa490@users.noreply.github.com>
2024-03-31 22:03:58 +0700

    Create Genlib.java

---
+++ Genlib.java
@@ -0,0 +1,10 @@
+public class Genlib {
+    public static <T extends Number,X extends Number> void  add (T a, X b) {
+        stem.out.println(a.double value() + b.double levalue());
+    }
+
+    public static <A,B> void printAlamat(A a, Bb = "" + b){
+        system.out.println(a + " " + b);
+    }
+
+}
